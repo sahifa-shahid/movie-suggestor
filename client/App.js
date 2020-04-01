40d90c9caf39a43eefbee787d0b80b2e25ef48cf
SahifaShahid <sahifashahid2626@gmail.com>
2020-03-31 21:28:39 -0400

    LETS GO SARIM

--- client/App.js
+++ client/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import logo from './assets/logo.svg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <LinearGradient colors={["rgba(0,0,0,0.95)", "#4e4e4e", "rgba(0,0,0,0.95)"]} style={styles.background}>
+        <Image source={logo} style={styles.logo} resizeMode="contain"/>
+        <Text style={{color: 'white', fontFamily: 'Raleway-Regular', fontSize: '72px'}}>SCOUT</Text>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    height: '100vh',
+    backgroundColor: 'white',
+  },
+  
+  logo: {
+    width: "13%", 
+    flex: 1, 
+    marginLeft: '10%',
+  },
+  
+  appTitle: {
+    font: "R"
   },
 });
